133ed462d6d96675545e86418a0b866969dd9a38
tomohiro <michy0221m@gmail.com>
2020-07-15 20:28:53 +0900

    userController修正

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Pagination\Environment;
 use App\User;
 use App\Post;
 use Auth;
