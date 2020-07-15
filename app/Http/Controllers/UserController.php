ecdffd30f9979999029d7a908481107fd716cfbc
tomohiro <michy0221m@gmail.com>
2020-07-15 20:53:31 +0900

    added InputClass

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Pagination\Environment;
+use Illuminate\Support\Facades\Input;
 use App\User;
 use App\Post;
 use Auth;
