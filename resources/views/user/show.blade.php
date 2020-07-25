7f98dd968a3af438082acc7143f416a3b0aa404e
tomohiro <michy0221m@gmail.com>
2020-07-25 18:57:56 +0900

    修正です

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -40,26 +40,30 @@
                  @if(!isset($posts))
                      <p>登録したurlはありません</p>
                  @else
-                      @foreach($posts as $post)
-                          <div class = "target-container">
-                             <span class = "content-title">
-                                 {{$post->title}}
-                                 @if($user->id == Auth::id())
-                                  <a href="post_edit/{{$post->id}}" class = "edit-btn"><i class=" fas fa-edit"></i></a>
-                                  @endif
-                             </span>
-                             <div class = "btn-area"><span class = "detail_btn" unselectable="on">リンク詳細切替</span></div>
-                              <div class = "slide-area hide">
-                                  <div><span class = "content-text">{{$post->content}}</span></div>
-                                  <div class = "url-area">
-                                      <a href="{{$post->target}}">{{$post->target}}</a>
-                                  </div>
-                                  <div class = "copy-btn-area">
-                                      <span class = "copy_btn" unselectable = "on">urlをコピー<span>
-                                  </div>
-                              </div>
-                          </div>
-                      @endforeach
+                     @foreach($posts as $post)
+                         <div class = "target-container">
+                             <div class = "title-wrapper">
+                                <div class = "titleArea">
+                                     <span class = "content-title">
+                                         @if($user->id == Auth::id())
+                                            <a href="post_edit/{{$post->id}}" class = "edit-btn"><i class=" fas fa-edit"></i></a>
+                                         @endif
+                                        {{$post->title}}
+                                    </span>
+                                </div>
+                                <div class = "btn-area"><span class = "detail_btn" unselectable="on">詳細表示</span></div>
+                             </div>
+                             <div class = "slide-area hide">
+                                 <div><span class = "content-text">{{$post->content}}</span></div>
+                                 <div class = "url-area">
+                                     <a href="{{$post->target}}">{{$post->target}}</a>
+                                 </div>
+                                 <div class = "copy-btn-area">
+                                     <span class = "copy_btn" unselectable = "on">urlをコピー<span>
+                                 </div>
+                             </div>
+                         </div>
+                     @endforeach
                  @endif
            
             </div>
